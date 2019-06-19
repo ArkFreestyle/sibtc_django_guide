a475345a0c9c7b61e5a7fceff68f08581d7f46f3
ARK <ArkFreestyle@yahoo.com>
2019-06-19 16:39:13 +0500

    Completed 2 and half tutorials, calling it a day and pushing all progress!

--- src/myproject/boards/tests.py
+++ src/myproject/boards/tests.py
@@ -1,20 +1,66 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from .views import home
+from .views import home, board_topics
+from .models import Board
 
 
 class HomeTests(TestCase):
     """Class for all home page related tests"""
 
+    def setUp(self):
+        self.board = Board.objects.create(name="Django", description="Django board.")
+        url = reverse('home')
+        self.response = self.client.get(url)
+
     def test_home_view_status_code(self):
         """Response returned should be 200"""
         
-        url = reverse('home')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(self.response.status_code, 200)
 
     def test_home_url_resolves_home_view(self):
         """Root url (/) should return the home view"""
 
         view = resolve('/')
         self.assertEquals(view.func, home)
+    
+    def test_home_view_contains_links_to_topics_page(self):
+        """Clicking home should link to home"""
+
+        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
+        self.assertContains(self.response, 'href="{}"'.format(board_topics_url))
+
+
+class BoardTopicsTests(TestCase):
+    """Class for all Board related tests"""
+
+    def setUp(self):
+        """Create a test board"""
+
+        Board.objects.create(name='Django', description='Django Board.')
+
+    def test_board_topics_view_success_status_code(self):
+        """Response returned should be 200"""
+
+        url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_board_topics_view_not_found_status_code(self):
+        """Response should be 404"""
+
+        url = reverse('board_topics', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    def test_board_topics_url_resolves_board_topics_view(self):
+        """Testing whether the correct view function is being used"""
+
+        view = resolve('/boards/1/')
+        self.assertEquals(view.func, board_topics)
+
+    def test_board_topics_view_contains_link_back_to_homepage(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(board_topics_url)
+        homepage_url = reverse('home')
+        self.assertContains(response, 'href="{}"'.format(homepage_url))
+        
--- src/myproject/boards/views.py
+++ src/myproject/boards/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Board
 
 # Create your views here.
 def home(request):
     """Any view function takes a request, and returns a response"""
+    
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
+    
+
+def board_topics(request, pk):
+    """Function renders the board topics page"""
+
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
+
+
+def new_topic(request, pk):
+    """View for creating new topics"""
+
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'new_topic.html', {'board': board})
     
