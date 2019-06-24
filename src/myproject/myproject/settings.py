f0b8073fa7d342b95f4bed7a79408efd63c415f4
arkfreestyle <arkfreestyle@yahoo.com>
2019-06-24 09:10:13 +0000

    Completed deployment to pythonanywhere and shifted project to MySQL instead of SQLite

--- src/myproject/myproject/settings.py
+++ src/myproject/myproject/settings.py
@@ -78,13 +78,21 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+      'default': {
+          'ENGINE': 'mysql.connector.django',
+          'NAME': 'arkfreestyle$test',
+          'USER': 'arkfreestyle',
+          'PASSWORD': 'testing123',
+          'HOST': 'arkfreestyle.mysql.pythonanywhere-services.com',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
